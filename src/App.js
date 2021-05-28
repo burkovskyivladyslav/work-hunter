6714e1c35048af5964117eab53912fb86ca5dc47
burkovskyivlad <burkovskyivlad@outlook.com>
2021-05-28 23:35:57 +0600

    wrong code dedected

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './App.css';
 import Header from './Header';
@@ -9,9 +10,62 @@ function App() {
     <div className="app">
       {/* Header */}
       <Header/>
+<<<<<<< HEAD
     
     <div className="app__body">
          <Sidebar/>
+=======
+
+<<<<<<< HEAD
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+          <h1>hello this is ujjal zaman</h1>
+
+          <h5>added anohter</h5>
+        </span>
+      </header>
+=======
+      {/* App body */}
+      {/* Side bar */}
+      {/* Feed */}
+      {/* Widgets */}
+>>>>>>> d0a91ac752ff50f61084ef8151fc98357ce8019d
+>>>>>>> 582325a9b857c9d7af830e132f59e5247c5299f5
     </div>
    
       </div> 
