f79cbdd0a69ac00c3504fedd9ac45070d906ffc9
Sultana Tasnim Jahan <tasnim047sultana@gmail.com>
2021-05-28 18:26:23 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,68 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <h1>I am sakib ahmed</h1>
-        <h2>I am sakib </h2>
-        <p>
-       
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <h1>hello there this is me</h1>
-        </div>
+    <div className="app">
+      {/* Header */}
+      <Header/>
 
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          <h1>hello this is ujjal zaman</h1>
-        </span>
-      </header>
-      <div>
-        <h1>testing</h1>
-        <h1>my commit</h1>
-      </div>
+      {/* App body */}
+      {/* Side bar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
