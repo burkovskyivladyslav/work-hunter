47ee0530749d88d11c9bada2a923484836a112e2
burkovskyivlad <burkovskyivlad@outlook.com>
2021-05-28 22:17:45 +0600

    html added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import {Avatar} from "@material-ui/core"
 const Sidebar = () => {
     return (
         <div className="sidebar">
-        <h1>This is sidebar</h1>
+        <div className="sidebar__top">
+            <img src="" alt="" />
+            <Avatar className="sidebar__avatar"/>
+            <h2>Sakib Ahmed</h2>
+            <h4>React Developer</h4>
+            <div className="sidebar__stats">
+            <div className="sidebar__state">
+                <p>Who viewed your profile</p>
+                <p className="sidebar__stateNumber">234</p>
+            </div>
+            <div className="sidebar__state">
+                <p>Views on post</p>
+                <p className="sidebar__stateNumber">234</p>
+            </div>
+
+            </div>
+
+
+        </div>
             
         </div>
     );
