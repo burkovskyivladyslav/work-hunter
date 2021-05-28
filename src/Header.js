cc331634b7ef4c024658b9233140f668d6bde61e
Sultana Tasnim Jahan <tasnim047sultana@gmail.com>
2021-05-28 20:42:34 +0600

    header done

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import './Header.css'
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
         <div className="header">
-            <h1>This is the header</h1>
-
             <div className="header_left">
-                <img src="" alt="" />
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="" />
 
                 <div className="header_search">
-                    {/* searchIcon */}
+                    <SearchIcon></SearchIcon>
                     <input type="text" name="" id="" />
                 </div>
             </div>
 
             <div className="header_right">
-
+                    <HeaderOption Icon={HomeIcon} title="Home"/>
+                    <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                    <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                    <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                    <HeaderOption Icon={NotificationsIcon} title="Notification"/>
+                    <HeaderOption avatar="https://i.ibb.co/Gdx7FY5/Sultana-Tasnim-Jahan.jpg" title="Me"/>
             </div>
         </div>
     )
