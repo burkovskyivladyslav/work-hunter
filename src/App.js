46724ced1938b72a071f2b5e6168fdd914ad6f82
unknown <ujjal>
2021-05-28 23:31:09 +0600

    added header by tasnim

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './App.css';
 import Header from './Header';
@@ -9,55 +8,10 @@ function App() {
       {/* Header */}
       <Header/>
 
-<<<<<<< HEAD
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          <h1>hello this is ujjal zaman</h1>
-
-          <h5>added anohter</h5>
-        </span>
-      </header>
-=======
       {/* App body */}
       {/* Side bar */}
       {/* Feed */}
       {/* Widgets */}
->>>>>>> d0a91ac752ff50f61084ef8151fc98357ce8019d
     </div>
   );
 }
