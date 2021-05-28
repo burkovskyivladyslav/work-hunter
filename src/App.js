bc394a87a6c2d6822e75d37331779ad37bbb79a9
burkovskyivlad <burkovskyivlad@outlook.com>
2021-05-28 23:03:22 +0600

    some code added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
+
 function App() {
   return (
     <div className="app">
       {/* Header */}
-      <Header>
-      </Header>
-      <Sidebar/>
+      <Header/>
+    
+    <div className="app__body">
+         <Sidebar/>
+    </div>
+   
       </div> 
      
   )
